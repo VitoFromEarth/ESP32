61b6a4e5ae1a0c989beab4c0a67de9d1cd0edcf9
Vitalii Kononenko <vitalii@getruck.co>
2026-08-20 09:20:34 +0200

    chore: generate build artifacts for Rust project dependencies

---
+++ lessons/lesson_01_sos/rust/target/debug/build/proc-macro2-eb9428bbe2dee0f0/build_script_build-eb9428bbe2dee0f0.d
@@ -0,0 +1,5 @@
+C:\Users\vitof\Documents\PlatformIO\Projects\ESP32\lessons\lesson_01_sos\rust\target\debug\build\proc-macro2-eb9428bbe2dee0f0\build_script_build-eb9428bbe2dee0f0.d: C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.107\build.rs
+
+C:\Users\vitof\Documents\PlatformIO\Projects\ESP32\lessons\lesson_01_sos\rust\target\debug\build\proc-macro2-eb9428bbe2dee0f0\build_script_build-eb9428bbe2dee0f0.exe: C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.107\build.rs
+
+C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.107\build.rs:
---
+++ lessons/lesson_01_sos/rust/target/debug/deps/esp_metadata_generated-dff44427ea03e953.d
@@ -0,0 +1,9 @@
+C:\Users\vitof\Documents\PlatformIO\Projects\ESP32\lessons\lesson_01_sos\rust\target\debug\deps\esp_metadata_generated-dff44427ea03e953.d: C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\lib.rs C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\_generated_esp32.rs C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\_build_script_utils.rs
+
+C:\Users\vitof\Documents\PlatformIO\Projects\ESP32\lessons\lesson_01_sos\rust\target\debug\deps\libesp_metadata_generated-dff44427ea03e953.rlib: C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\lib.rs C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\_generated_esp32.rs C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\_build_script_utils.rs
+
+C:\Users\vitof\Documents\PlatformIO\Projects\ESP32\lessons\lesson_01_sos\rust\target\debug\deps\libesp_metadata_generated-dff44427ea03e953.rmeta: C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\lib.rs C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\_generated_esp32.rs C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\_build_script_utils.rs
+
+C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\lib.rs:
+C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\_generated_esp32.rs:
+C:\Users\vitof\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\esp-metadata-generated-0.4.0\src\_build_script_utils.rs:
